9575ccc8c436d53013e6250ce266f28bc16314db
CDavidDv <carlos.david1806@gmail.com>
2024-02-01 03:25:02 -0600

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+// tailwind.config.js
+
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			
+		},
+	  screens: {
+		'sm': '640px',     
+		'md': '912px',    
+		'lg': '1030px',    
+		'xl': '1280px',    
+		'2xl': '1536px',   
+	  },
+	},
+	plugins: [],
+  }
+  
